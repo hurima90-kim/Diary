1b36a749914e207c1a9fc03eec4878c06d9f0402
김은태 <jason_kim@gim-euntaeui-MacBookPro.local>
2021-07-20 14:12:17 -0700

    Style: setting globalStyle & folder tree

---
+++ src/components/Main/ProfileImage.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from 'react';
+import styled from '@emotion/styled';
+
+const PROFILE_IMAGE_LINK =
+  '<https://avatars.githubusercontent.com/u/66353903?v=4>';
+
+const ProfileImageWrapper = styled.img``;
--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,7 +1,35 @@
 import React, { FunctionComponent } from 'react';
 import { graphql } from 'gatsby';
-import Text from 'components/Text';
-import { Link } from 'gatsby';
+import styled from '@emotion/styled';
+import { Global, css } from '@emotion/react';
+
+const globalStyle = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    font-size: 20px;
+  }
+`;
+
+const TextStyle = css`
+  font-size: 18px;
+  font-weight: 700;
+  color: gray;
+`;
+
+const Text1 = styled.div<{ disable: boolean }>`
+  font-size: 20px;
+  font-weight: 700;
+  text-decoration: ${({ disable }) => (disable ? 'line-through' : 'none')};
+`;
+
+const Text2 = styled('div')<{ disable: boolean }>(({ disable }) => ({
+  fontSize: '15px',
+  color: 'blue',
+  textDecoration: disable ? 'line-through' : 'none',
+}));
 
 interface InfoPageProps {
   data: {
@@ -24,10 +52,11 @@ const InfoPage: FunctionComponent<InfoPageProps> = function ({
 }) {
   return (
     <div>
-      <Text text={title} />
-      <Text text={description} />
-      <Text text={author} />
-      <Link to="/">To Main</Link>
+      {console.log(title)}
+      <Global styles={globalStyle} />
+      <div css={TextStyle}>{title}</div>
+      <Text1 disable={true}>{description}</Text1>
+      <Text2 disable={true}>{author}</Text2>
     </div>
   );
 };
@@ -38,7 +67,7 @@ export const metadataQuery = graphql`
   {
     site {
       siteMetadata {
-        author
+        title
         description
         author
       }
