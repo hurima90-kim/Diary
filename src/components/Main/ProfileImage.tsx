1ae0e129896ef9d75aeeb172d927b459a1d1a7d8
hurima90-kim <hurima90@gmail.com>
2021-07-20 14:25:31 -0700

    Style: Add profile image from github

--- src/components/Main/ProfileImage.tsx
+++ src/components/Main/ProfileImage.tsx
@@ -1,7 +1,17 @@
 import React, { FunctionComponent } from 'react';
 import styled from '@emotion/styled';
 
-const PROFILE_IMAGE_LINK =
-  '<https://avatars.githubusercontent.com/u/66353903?v=4>';
+const PROFILE_IMAGE_LINK = `https://avatars.githubusercontent.com/u/66353903?v=4`;
 
-const ProfileImageWrapper = styled.img``;
+const ProfileImageWrapper = styled.img`
+  width: 120px;
+  height: 120px;
+  margin-bottom: 30px;
+  border-radius: 50%;
+`;
+
+const ProfileImage: FunctionComponent = function () {
+  return <ProfileImageWrapper src={PROFILE_IMAGE_LINK} alt="Profile Image" />;
+};
+
+export default ProfileImage;
