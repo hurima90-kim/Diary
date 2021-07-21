f5f19624e8be17426c6f6e5866058b28a9b19295
hurima90-kim <hurima90@gmail.com>
2021-07-20 22:28:41 -0700

    Create categoryList

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,20 @@
 import React, { FunctionComponent } from 'react';
 import styled from '@emotion/styled';
 import GlobalStyle from 'components/Common/GlobalStyle';
-import ProfileImage from 'components/Main/ProfileImage';
-import Introduction from 'components/Main/Introduction';
 import Footer from 'components/Common/Footer';
+import CategoryList from 'components/Main/CategoryList';
+import Introduction from 'components/Main/Introduction';
+
+const CATEGORY_LIST = {
+  All: 5,
+  Web: 3,
+  Mobile: 2,
+};
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100%;
+  height: 100vh;
 `;
 
 const IndexPage: FunctionComponent = function () {
@@ -16,6 +22,7 @@ const IndexPage: FunctionComponent = function () {
     <Container>
       <GlobalStyle />
       <Introduction />
+      <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
       <Footer />
     </Container>
   );
